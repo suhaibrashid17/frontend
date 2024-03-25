03362566cf06672d04fd3e6bb721c0c19b018968
AbdulTawabJuly <tawabmasood31july@gmail.com>
2024-03-25 22:00:00 +0530

    Stripe Done

--- src/pages/StripeCheckout.js
+++ src/pages/StripeCheckout.js
@@ -8,28 +8,30 @@ import { useSelector } from "react-redux";
 import { selectCurrentlyClickedRoom } from "../features/rooms/RoomSlice";
 import { useNavigation } from "react-router-dom";
 
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
-// const stripePromise = loadStripe("pk_test_51NoQGmSDkACrq3oOH97IaDGRCc9BtOEfzPsL6qga6XSkwPoFLD35ifp8QuDEpFBgStRAcPlS9K61rTBl8nZ3b4DK007jTkfUub");
-const stripePromise=loadStripe("pk_test_51NoQGmSDkACrq3oOH97IaDGRCc9BtOEfzPsL6qga6XSkwPoFLD35ifp8QuDEpFBgStRAcPlS9K61rTBl8nZ3b4DK007jTkfUub")
+const stripePromise = loadStripe(
+  "pk_test_51NoQGmSDkACrq3oOH97IaDGRCc9BtOEfzPsL6qga6XSkwPoFLD35ifp8QuDEpFBgStRAcPlS9K61rTBl8nZ3b4DK007jTkfUub"
+);
 export default function StripeCheckout() {
   const [clientSecret, setClientSecret] = useState("");
-  const currentlySelectedPaidRoom = useSelector(selectCurrentlyClickedRoom)
+  const currentlySelectedPaidRoom = useSelector(selectCurrentlyClickedRoom);
+  console.log("Current Price",currentlySelectedPaidRoom.price)
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     fetch("http://localhost:8080/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: currentlySelectedPaidRoom.name }),
+      body: JSON.stringify( {totalAmount : currentlySelectedPaidRoom.price} ),
+      // meta: {
+      //   room_id: currentlySelectedPaidRoom.id,
+      // },
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
   }, []);
 
   const appearance = {
-    theme: 'stripe',
+    theme: "stripe",
   };
   const options = {
     clientSecret,
